0807401331b1292be9c90c4f2441478faf5fe20a
Saket Sharma <ricky.saket@gmail.com>
2025-03-16 11:18:54 -0500

    Java Examples- Java concepts

--- src/main/java/org/example/java/lambda/LambdaExample.java
+++ src/main/java/org/example/java/lambda/LambdaExample.java
@@ -1,4 +1,4 @@
-package org.example.java;
+package org.example.java.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/org/example/java/streams/StreamExample.java
+++ src/main/java/org/example/java/streams/StreamExample.java
@@ -1,4 +1,4 @@
-package org.example.java;
+package org.example.java.streams;
 
 import java.util.ArrayList;
 import java.util.Arrays;
